73b11ef7f2430b45f897a4364caf9637bdf56801
aquibm225 <aquibmustafa.225@gmail.com>
2015-12-08 22:06:45 +0530

    indentation

--- parseCMD.c
+++ parseCMD.c
@@ -1,19 +1,26 @@
 //run as : gcc parseCMD.c -o a
-#include <stdio.h> 							// scanf, printf
+#include <stdio.h> 					// scanf, printf
 #include <stdbool.h> 						// bool
 #include <stdlib.h> 						// malloc
+
+char *data; 								// arrey of desired string
+int state = -1; 							// state of SM
+int numberofchar; 							// number of elements in desired string
+
+
 
 //Pilot program
 int main()
 {
-	char c;								// incoming character		
-	bool helloFound = false;					// success flag					
-	FILE *inputfile;						// input file handler
-	inputfile = fopen("test.txt", "r+");		// open input file 
+	char c;						// incoming character		
+	bool helloFound = false;					// success flag			
+	data = getdata();		
+	FILE *inputfile;				// input file handler
+	inputfile = fopen("test.txt", "r+");			// open input file 
 	if (inputfile == NULL) 					// error in opening file
 	{
 		printf("No input file\n");			// print error
-		return 0;							// exit
+		return 0;					// exit
 	}
 	for(;;)								// run infinitely
 	{
